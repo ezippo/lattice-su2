6d68f8d82c582aa23110ca2eca6aa0eeed7d979c
Emanuele Zippo <emanuele.zippo@gmail.com>
2021-04-21 23:37:25 +0200

    1.1.1

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("C:\\Users\\e.zippo\\Desktop\\Università\\Tesi\\codice_lattice_su2\\lattice-su2")
+sys.path.append("C:\\Users\\e.zippo\\Desktop\\Università\\Tesi\\codice_lattice_su2\\lattice-su2\\data_analysis")
 
 import numpy as np
 from matplotlib import pyplot as pyp
@@ -8,8 +8,8 @@ from scipy.optimize import curve_fit
 from data_analysis import analysis_functions as ana_func
 
 if __name__=='__main__':
-    dati = np.loadtxt("C:\\Users\\e.zippo\\Desktop\\Università\\Tesi\\codice_lattice_su2\\su2-1.0.0\\dati.dat")
-    n_obs = 3
+    dati = np.loadtxt("C:\\Users\\e.zippo\\Desktop\\Università\\Tesi\\codice_lattice_su2\\lattice-su2\\su2-1.1.0\\dati.dat")
+    n_obs = 1
     mu1 = ana_func.average(dati[:,n_obs])
     sigma1 = ana_func.sigma_bootstrap(dati[:,n_obs], 3, 300)
     print(mu1, sigma1)
--- su2-1.1.0/include/myrngparam.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYRNGPARAM_H
-#define MYRNGPARAM_H
-
-#define DSFMT_MEXP 132049
-
-#endif
