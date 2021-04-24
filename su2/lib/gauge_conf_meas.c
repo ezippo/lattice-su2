9a766b979e0f2870ef5756d7e50949b89873c5a2
Emanuele Zippo <emanuele.zippo@gmail.com>
2021-04-24 18:42:40 +0200

    1.2.0 : wilson_loop

--- su2/lib/gauge_conf_meas.c
+++ su2/lib/gauge_conf_meas.c
@@ -57,6 +57,94 @@ double plaquettep(Gauge_Conf const * const GC,
    }
 
 
+// computation of the rectangulare wilson loop (1/NCOLOR the trace of) size_i*size_j in position r and positive directions i,j
+double wilson_loopp(Gauge_Conf const * const GC,
+                    Geometry const * const geo,
+                    GParam const * const param,
+                    long r,
+                    int i,
+                    int j,
+                    int const size_i,
+                    int const size_j)
+{
+  Su2 matrix_loop, matrix_43;
+  long r_new12, r_new43;
+  int count;
+
+  #ifdef DEBUG
+    if(r >= param->d_volume)
+     {
+     fprintf(stderr, "r too large: %ld >= %ld (%s, %d)\n", r, param->d_volume, __FILE__, __LINE__);
+     exit(EXIT_FAILURE);
+     }
+    if(j >= STDIM || i >= STDIM)
+     {
+     fprintf(stderr, "i or j too large: (i=%d || j=%d) >= %d (%s, %d)\n", i, j, STDIM, __FILE__, __LINE__);
+     exit(EXIT_FAILURE);
+     }
+  #else
+    (void) param; // just to avoid warning at compile time
+  #endif
+
+//
+//           ^ i
+//           |     (3)
+// r+size_i  +------<------+
+//           |             |
+//       (4) V             ^ (2)
+//           |             |
+//           +------>------+---> j
+//         r      (1)     r+size_j
+//
+
+  // degenerate rectangular case: loop = Id
+  if(size_i==0 || size_j==0)  return 1.0;
+
+  // (1)
+  equal(&matrix_loop, &(GC->lattice[r][j]));              // M=U_j(r)
+  r_new12 = nnp(geo,r,j);                                   // r -> r+j
+  for(count=1; count<size_j; count++)
+  {
+    times_equal(&matrix_loop, &(GC->lattice[r_new12][j]));   // M=U_j(r)U_j(r+j)...U_j(r+count*j)
+    r_new12 = nnp(geo,r_new12,j);
+  }
+
+  // (2)
+  for(count=0; count<size_i; count++)
+  {
+    times_equal(&matrix_loop, &(GC->lattice[r_new12][i]));   // M=(1)*U_i(r+size_j)...U_i(r+size_j+count*i)
+    r_new12 = nnp(geo,r_new12,i);
+  }
+
+  // (4)
+  equal(&matrix_43, &(GC->lattice[r][i]));              // M43= U_i(r)
+  r_new43 = nnp(geo,r,i);
+  for(count=1; count<size_i; count++)
+  {
+    times_equal(&matrix_43, &(GC->lattice[r_new43][i]));   // M43=U_i(r)U_i(r+i)...U_i(r+count*i)
+    r_new43 = nnp(geo,r_new43,i);
+  }
+
+  // (3)
+  for(count=0; count<size_j; count++)
+  {
+    times_equal(&matrix_43, &(GC->lattice[r_new43][j]));   // M43=(4)*U_j(r+size_i)...U_j(r+size_i+count*j)
+    r_new43 = nnp(geo,r_new43,j);
+  }
+
+  #ifdef DEBUG
+    if(r_new12!=r_new43)
+    {
+      fprintf(stderr, "Error in Wilson loop computation: loop seems not to be rectangular...\n r12=%ld , r43=%ld \n", r_new12, r_new43);
+      exit(EXIT_FAILURE);
+    }
+  #endif
+
+  times_equal_dag(&matrix_loop, &matrix_43);      // M=(1)*(2)*( (4)(3) )^dag
+  return retr(&matrix_loop);
+}
+
+
 // compute the mean plaquettes (spatial, temporal)
 void plaquette(Gauge_Conf const * const GC,
                Geometry const * const geo,
@@ -165,6 +253,56 @@ void plaquette_adj(Gauge_Conf const * const GC,
   }
 
 
+// compute the mean wilson loop size_i*size_j (spatial, temporal)
+void wilson_loop(Gauge_Conf const * const GC,
+                 Geometry const * const geo,
+                 GParam const * const param,
+                 int const size_i,
+                 int const size_j,
+                 double *wloop_s,
+                 double *wloop_t)
+{
+  long r;
+  double ws=0.0, wt=0.0;
+
+  #ifdef OPENMP_MODE
+  #pragma omp parallel for num_threads(NTHREADS) private(r) reduction(+ : wt) reduction(+ : ws)
+  #endif
+  for(r=0; r<(param->d_volume); r++)
+     {
+     int i, j;
+     i=0;
+     for(j=1; j<STDIM; j++)
+        {
+        wt+=wilson_loopp(GC, geo, param, r, i, j, size_i, size_j);
+        }
+
+     for(i=1; i<STDIM; i++)
+        {
+        for(j=i+1; j<STDIM; j++)
+           {
+           ws+=wilson_loopp(GC, geo, param, r, i, j, size_i, size_j);
+           }
+        }
+     }
+
+  if(STDIM>2)
+    {
+    ws*=param->d_inv_vol;
+    ws/=((double) (STDIM-1)*(STDIM-2)/2);
+    }
+  else
+    {
+    ws=0.0;
+    }
+
+  wt*=param->d_inv_vol;
+  wt/=((double) STDIM-1);
+
+  *wloop_s=ws;
+  *wloop_t=wt;
+}
+
 
 // compute the mean Polyakov loop (the trace of)
 void polyakov(Gauge_Conf const * const GC,
@@ -211,21 +349,22 @@ void perform_measures_localobs(Gauge_Conf const * const GC,
                                GParam const * const param,
                                FILE *datafilep)
    {
-   double plaqs, plaqt, polyre, polyim;
+   double plaqs, plaqt, wloops, wloopt, polyre, polyim;
 
    plaquette(GC, geo, param, &plaqs, &plaqt);
+   wilson_loop(GC, geo, param, 2, 2, &wloops, &wloopt);
    polyakov(GC, geo, param, &polyre, &polyim);
 
    if(fabs(param->d_adj_beta)<MIN_VALUE)
       {
-      fprintf(datafilep, "%.12g %.12g %.12g %.12g ", plaqs, plaqt, polyre, polyim);
+      fprintf(datafilep, "%.12g %.12g %.12g %.12g %.12g %.12g ", plaqs, plaqt, wloops, wloopt, polyre, polyim);
       fprintf(datafilep, "\n");
       }
    else
       {
       double plaqs_adj, plaqt_adj;
       plaquette_adj(GC, geo, param, &plaqs_adj, &plaqt_adj);
-      fprintf(datafilep, "%.12g %.12g %.12g %.12g %.12g %.12g ", plaqs, plaqt, plaqs_adj, plaqt_adj, polyre, polyim);
+      fprintf(datafilep, "%.12g %.12g %.12g %.12g %.12g %.12g %.12g %.12g ", plaqs, plaqt, wloops, wloopt, plaqs_adj, plaqt_adj, polyre, polyim);
       fprintf(datafilep, "\n");
       }
    fflush(datafilep);
